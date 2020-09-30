fdeda2d19fcd9bb256879bfa7534e1c2c16b1c0c
Saber-Bjeoui <bjeoui.saber@gmail.com>
2020-09-30 10:51:17 +0100

    attempt to include home template

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,21 +8,7 @@ import Home from "./components/home.jsx";
 export class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <div className="jumbotron">
-          <h1 className="display-4">From developpers to developpers</h1>
-          <p className="lead">
-            A tool for teams and organisation to success your project
-          </p>
-          <hr className="my-4" />
-          <button type="button" class="btn btn-primary btn-lg">
-            Large button
-          </button>
-          <button type="button" class="btn btn-secondary btn-lg">
-            Large button
-          </button>
-        </div>
-      </div>
+      <Signup />
     );
   }
 }
