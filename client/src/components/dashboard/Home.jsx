d41a637e42c08823e3371e856a797ecd14e87674
Saber-Bjeoui <bjeoui.saber@gmail.com>
2020-09-30 22:39:32 +0100

    commit before merging frontend branch

---
+++ client/src/components/dashboard/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Home extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="h3 mb-4 text-gray-800">Home</h1>
+        <p>this is home</p>
+      </div>
+    );
+  }
+}
+export default Home;
